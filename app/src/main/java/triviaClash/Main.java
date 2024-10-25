e732ad138a29dc9ae605701ad4d74e4c361e7ca9
programmer <leolegendnarasimha@gmail.com>
2024-10-25 19:20:36 +0530

    Fixed bugs in connection
    - Removed unwanted folders

--- app/src/main/java/triviaClash/Main.java
+++ app/src/main/java/triviaClash/Main.java
@@ -1,15 +1,14 @@
 package triviaClash;
 
-import triviaClash.utilities.ConnectionManager;
-import triviaClash.Data.RequestMethod;
-
 import org.json.JSONObject;
+
+import triviaClash.Data.RequestMethod;
+import triviaClash.utilities.ConnectionManager;
 
 public class Main {
     public static void main(String args[]) {
-        ConnectionManager manager = new ConnectionManager();
-        manager.connect("https://opentdb.com/api.php?amount=10",RequestMethod.GET);
-        JSONObject jsonData = manager.fetchData();
-        System.out.println(jsonData);
+        ConnectionManager conman = new ConnectionManager(); 
+        conman.connect("https://opentdb.com/api.php?amount=4",RequestMethod.GET);
+        System.out.println(conman.getJsonData());
     }
 }
--- app/src/main/java/triviaClash/utilities/ConnectionManager.java
+++ app/src/main/java/triviaClash/utilities/ConnectionManager.java
@@ -1,65 +1,55 @@
 package triviaClash.utilities;
 
-// Custom Classes
-import triviaClash.Data.RequestMethod;
-
-// Java built-in classes
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.IOException; 
 import java.io.InputStreamReader;
 import java.net.URL; 
-import java.net.HttpURLConnection;
-import org.json.JSONObject;
-import org.json.HTTP;
+import java.net.HttpURLConnection; 
+
+import triviaClash.Data.RequestMethod;
+
+import org.json.JSONObject; 
 
 public class ConnectionManager {
-    private URL url;
-    private int responseCode;
-    private JSONObject responseData;
-    private HttpURLConnection connection;
     
+    private URL url;
+    private JSONObject jsonData; 
+    private HttpURLConnection connection;
+    private int responseCode; 
 
-    public void connect(String url, RequestMethod method) {
-        method = RequestMethod.GET;
+    public HttpURLConnection connect(String url, RequestMethod method) {
         try {
-            connection = (HttpURLConnection)this.url.openConnection();
-            responseCode = connection.getResponseCode();
-            if(responseCode == connection.HTTP_OK) {
-                System.out.println("Connected successfully !!");
-                switch(method) {
-                case GET: 
-                    System.out.println("Setting request method to GET...");
+            this.url = new URL(url);
+            connection = (HttpURLConnection) this.url.openConnection();
+            switch(method) {
+                case GET:
                     connection.setRequestMethod("GET");
-
-                    StringBuilder responseString = new StringBuilder();
-                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                
-                    String inputLine;
-                    while((inputLine = in.readLine()) != null) {
-                    responseString.append(inputLine);
-                    }
-                    responseData = new JSONObject(responseString.toString());
                     break;
-
                 case POST:
-                    System.out.println("Setting request method to POST...");
                     connection.setRequestMethod("POST");
                     break;
-            
                 default:
-                    System.out.println("Using request method GET (default)...");
-                    break;
-                }
-            } else {
-                System.out.println("Connection failed !!");
+                    connection.setRequestMethod("GET");
             }
-        } catch (Exception e) {
-            System.out.println("Something went wrong !!\n" + e.getStackTrace());
 
+            responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                jsonData = new JSONObject(response.toString());
+            }
+        } catch(Exception e) {
+            System.out.println("Connection error !!");
         }
-    } 
+        return connection;
+    }
 
-    public JSONObject fetchData() {
-        return responseData;
+    public JSONObject getJsonData() {
+        return jsonData;
     }
 }
