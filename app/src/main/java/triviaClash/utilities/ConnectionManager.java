6170745c37195c662d145430d83b459344a62abc
programmer <leolegendnarasimha@gmail.com>
2024-11-04 20:39:06 +0530

    connect() now returns the status of connection ( i.e boolean ) , also initial HTTP Request codes check is implemented. Fixed response_code not found in JSONObject error by adding a key check.

--- app/src/main/java/triviaClash/utilities/ConnectionManager.java
+++ app/src/main/java/triviaClash/utilities/ConnectionManager.java
@@ -1,12 +1,9 @@
 package triviaClash.utilities;
 
 import java.io.BufferedReader;
-import java.io.IOException; 
 import java.io.InputStreamReader;
 import java.net.URI; 
 import java.net.HttpURLConnection; 
-
-import triviaClash.Data.GameData.ResponseCodes;
 import triviaClash.Data.RequestMethod;
 import triviaClash.Data.CommandMode; 
 
@@ -20,13 +17,15 @@ public class ConnectionManager {
     private int responseCode; 
     private boolean connected = false;
 
-    public HttpURLConnection connect(String url, RequestMethod method) {
+    public boolean connect(String url, RequestMethod method) {
         try {
             uri = new URI(url);
             connection = (HttpURLConnection) this.uri.toURL().openConnection();
             connection.setRequestMethod(method.name());
             responseCode = connection.getResponseCode();
             if (responseCode == HttpURLConnection.HTTP_OK) {
+                connected = true;
+                System.out.printf("\n\n> HTTP Connection successful, Response code : %d \n\n", responseCode);
                 BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 String inputLine;
                 this.connected = true;
@@ -40,36 +39,31 @@ public class ConnectionManager {
         } catch(Exception e) {
             OutputManager.SOP("Sorry, connection failed with the following error...\n", CommandMode.CMD_LINE);
             e.printStackTrace();
+        } 
+
+        if(connected) {
+            if(jsonData.has("response_code")) {
+                System.out.println(jsonData+" \nhas response code : "); 
+                switch(jsonData.getInt("response_code")) {
+                    case 0:
+                        break;
+                    case 1:
+                        System.out.println("Sorry, no results for the query...");
+                    case 2:
+                        System.out.println("Invalid parameter...");
+                    case 3:
+                        System.out.println("Token is likely to be expired or invalid...");
+                    case 4:
+                        System.out.println("Please reset the token!!");
+                    case 5:
+                        System.out.println("Too many requests...");
+                }
+            }
         }
-        
-        switch(jsonData.getInt("response_code")) {
-            case 0:
-                System.out.println("connection success...");
-                connected = true;
-                break;
-            case 1:
-                System.out.println("Sorry, no results for the query...");
-            case 2:
-                System.out.println("Invalid parameter...");
-            case 3:
-                System.out.println("Token is likely to be expired or invalid...");
-            case 4:
-                System.out.println("Please reset the token!!");
-            case 5:
-                System.out.println("Too many requests...");
-            default:
-                System.out.println("Some unexpected error has occured !!");
-                connected = false;
-                return null;
-        }
-        return connection;
+        return connected;
     }
 
     public JSONObject getJsonData() {
         return jsonData;
-    }
-
-    public boolean isConnected() {
-        return this.connected;
     }
 }
