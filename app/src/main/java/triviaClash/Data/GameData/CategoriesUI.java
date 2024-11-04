ed3686205bdd1f1e5b1c41e22648dc5dbe9a8283
programmer <leolegendnarasimha@gmail.com>
2024-11-04 20:33:20 +0530

    Categories UI is now functional and can fetch categories from the api and print them

--- app/src/main/java/triviaClash/Data/GameData/CategoriesUI.java
+++ app/src/main/java/triviaClash/Data/GameData/CategoriesUI.java
@@ -1,23 +1,24 @@
 package triviaClash.Data.GameData;
 
-import java.util.HashMap;
 import java.util.Map; 
 import triviaClash.utilities.QueryManager;
 
 public class CategoriesUI {
     
-    private Map<Integer, String> categories;
-
+    private static Map<Integer, Map<Integer, String>> categories;     
+    
     public CategoriesUI() {
-
         QueryManager manager = new QueryManager();
         categories = manager.getCategories();
+        printCategories();
     }
 
-    private void printCategories() {
+    private static void printCategories() {
         System.out.println("====== CATEGORIES ======");
-        for(int x: categories.keySet()) {
-            System.out.printf("%d. %s\n",x, categories.getOrDefault(x,""));
-        }
+        for(Integer x: categories.keySet()) {
+            for( Integer m: categories.get(x).keySet()) {
+                System.out.printf("\n%d. %s\n", x, categories.get(x).get(m));  
+            }
+        } 
     }
 }
